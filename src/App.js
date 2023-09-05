a947ae1b28ad11d56abc5b1adb37b170aa890f78
Mustafa <mustafamengutayy@gmail.com>
2023-09-05 21:10:05 +0300

    feat: add home router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
 const App = () => {
   return (
-    <div className='mx-auto h-screen container'>
+    <div className='container mx-auto h-screen mt-7.5'>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
