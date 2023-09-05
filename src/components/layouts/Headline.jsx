d883280fd95067d76de211a2383aef6e68935fdb
Mustafa <mustafamengutayy@gmail.com>
2023-09-05 21:34:03 +0300

    feat: add headline

---
+++ src/components/layouts/Headline.jsx
@@ -0,0 +1,15 @@
+const Headline = () => {
+  return (
+    <div className="relative mt-6">
+      <p className="text-2.5 text-center absolute top-[44px] left-[936px]">
+        SINCE 1984
+      </p>
+      <h1 className="text-36 font-serif font-semibold text-center">
+        LOOM-LUXE
+      </h1>
+      <p className="text-2.5 absolute top-[162px] left-[364px]">NICOSIA</p>
+    </div>
+  );
+};
+
+export default Headline;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Header from '../components/layouts/Header';
+import Headline from '../components/layouts/Headline';
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Headline />
     </div>
   );
 };
