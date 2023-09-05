f85ca3016fc47100b34bbdd7f693c212f24f7224
Mustafa <mustafamengutayy@gmail.com>
2023-09-05 21:09:44 +0300

    feat: add header

---
+++ src/components/layouts/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <nav className="flex justify-between text-base">
+      <div>
+        <p className="font-semibold">LOOM-LUXE</p>
+      </div>
+      <p>16.SEPT.2023</p>
+      <p>COLLECTIONS</p>
+    </nav>
+  );
+};
+
+export default Header;
